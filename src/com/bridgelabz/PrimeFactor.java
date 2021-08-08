9a09f6e0a528e46daba931ffc678b1d516a388ab
sreelipta <sreelipta65@gmail.com>
2021-08-08 23:54:30 +0530

    Refactor: computes prime factors of the number

---
+++ src/com/bridgelabz/PrimeFactor.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PrimeFactor {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+        System.out.print("The Prime Factors of " + n + " are : ");
+        int i = 2;
+        while (n > 1) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                n = n / i;
+            } else
+                i++;
+        }
+        System.out.println(" ");
+        sc.close();
+    }
+}
