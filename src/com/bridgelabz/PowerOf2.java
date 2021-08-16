7af0cdba206fb96ffb4731617d93da7528799ef7
sreeliptaa <86236489+sreeliptaa@users.noreply.github.com>
2021-08-17 00:25:24 +0530

    Added: power of two calculated

--- src/com/bridgelabz/PowerOf2.java
+++ src/com/bridgelabz/PowerOf2.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class PowerOf2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of N");
+        System.out.println("Enter the number N");
         int N = sc.nextInt();
-        System.out.println("Powers of two till " + N + " are: ");
-        int power_of_two = 1;
+        System.out.println("Powers of two N + " are: ");
+        int powerOfTwo = 1;
         if (N > 0 && N < 31) {
             for (int i = 1; i <= N; i++) {
-                power_of_two = 2 * power_of_two;
-                System.out.println(power_of_two);
+                powerOfTwo = 2 * powerOfTwo;
+                System.out.println(powerOfTwo);
             }
         } else {
             System.out.println("INVALID ENTER");
