b6058dd4d729f82c5703e2161e3a921dbdd1b057
sreeliptaa <86236489+sreeliptaa@users.noreply.github.com>
2021-08-16 22:50:14 +0530

    Added: checking f=given no is even or odd

--- src/com/bridgelabz/EvenOddNumber.java
+++ src/com/bridgelabz/EvenOddNumber.java
@@ -3,17 +3,19 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class EvenOddNumber {
-    static void printResult(int number){
-        if(number%2==0){
+    public static void main(String[] args){
+        System.out.println("Enter a Number");
+        Scanner scanner = new Scanner(System.in);
+        int number= scanner.nextInt();
+        scanner.close();
+    
+        if (number % 2 == 0) {
             System.out.println("The Given No Is Even Number");
-        }else{
+        }
+        else
+        {
             System.out.println("The Given No Is Odd Number");
         }
     }
-    public static void main(String[] args){
-        System.out.println("Enter a Number");
-        Scanner sc =new Scanner(System.in);
-        int number= sc.nextInt();
-        printResult(number);
-    }
+    
 }
